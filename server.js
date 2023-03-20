b152fec66cddad89f8372939a0f9eee23b1ef1d2
xiangfengg <yongxiangfeng13@gmail.com>
2023-03-20 15:57:05 +0800

    feat:forms

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 //...farther down the page
 
-mongoose.connect("mongodb://127.0.0.1:27017/signup", {
+mongoose.connect("mongodb://127.0.0.1:27017/SYNC", {
   useNewUrlParser: true,
 });
 mongoose.connection.once("open", () => {
