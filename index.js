8048b11d00b228efd035e724162c9821de1723c0
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-23 21:16:54 +0800

    feat: more configurations. Please refer to the indiv github repos to learn what they're for

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-import express from "express"; // server library
-import bodyParser from "body-parser"; // process req body
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express'; // server library
+import bodyParser from 'body-parser'; // process req body
+import mongoose from 'mongoose'; // mongodb access
+import cors from 'cors'; // cross origin requests
+import dotenv from 'dotenv'; // env variables
+import multer from 'multer'; // uploading files locally
+import helmet from 'helmet'; // req safety
+import morgan from 'morgan'; // logins
+import path from 'path'; // comes with node so dont need to install
+import { fileURLToPath } from 'url'; // tgt with path, we can properly set the path when configuring directories
+
+// configurations
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url); // grab file url 
+const __dirname = path.dirname(__filename); // only when use type modules
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin'}));
+app.use(morgan('common'));
+app.use(bodyParser.json({limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
+app.use(cors()); //invoke cross origin resource sharing policies
+app.use('/assets', express.static(path.join(__dirname, 'public/assets'))); // sets the dir of where we keep the assests, which is locally.
+
+// file storage - refer to github repo for multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
