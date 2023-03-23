472b4d973028015dea68c4c70591e62cb90c0cd1
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-23 18:01:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"; // server library
+import bodyParser from "body-parser"; // process req body
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
