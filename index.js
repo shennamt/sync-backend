c1afd4c44c7836cf079ff79eaf88b88b7f5db816
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-24 15:21:48 +0800

    chore: added more configs based on package instructions

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'; // server library
+import bodyParser from 'body-parser'; // process req body
+import mongoose from 'mongoose'; // mongodb access
+import cors from 'cors'; // cross origin requests
+import dotenv from 'dotenv'; // env variables
+import multer from 'multer'; // uploading files locally
+import helmet from 'helmet'; // req safety
+import morgan from 'morgan'; // logins
+import path from 'path'; // comes with node so dont need to install
+import { fileURLToPath } from 'url'; // tgt with path, we can properly set the path when configuring directories
+
+// CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url); // grab file url 
+const __dirname = path.dirname(__filename); // only when use type modules
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin'}));
+app.use(morgan('common'));
+app.use(bodyParser.json({limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
+app.use(cors()); //invoke cross origin resource sharing policies
+app.use('/assets', express.static(path.join(__dirname, 'public/assets'))); // sets the dir of where we keep the assests, which is locally.
+
+// FILE STORAGE - CHECK MULTER GITHUB FOR MORE INFO
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
