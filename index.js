27399c315235e00d648becb84cb3c6e252844efa
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-24 09:33:03 +0800

    feat: mongodb cluster cloud set up

--- index.js
+++ index.js
@@ -34,3 +34,12 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+// mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
