8cdd18925770d607a942d3e2da89b5fd9bde03ec
xiangfengg <yongxiangfeng13@gmail.com>
2023-03-25 15:20:43 +0800

    meege into local branch

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -8,10 +8,10 @@ const logininUser = async (req, res) => {
 //signup user
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
-  //using the user model we are creating, taking into the two agurments email and password we put in the model
+
   try {
     const user = await user.signup(email, password);
-    // send back a response of email and user object^
+
     res.status(200).json({ email, user });
   } catch (error) {
     res.status(400).json({ error: error.message });
