3eb62064619a9ef18abb501bc8732634499bf929
aaronyeo007 <aaron.yeo@icloud.com>
2023-03-26 11:44:32 +0800

    feature: - added display to add or delete project member

--- models/Project.js
+++ models/Project.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const projectSchema = new mongoose.Schema({
   projectName: { type: String, required: true },
   agile: Boolean,
-  kanban: Boolean
+  kanban: Boolean,
+  member: { type: Array, default: [] }
 });
 
 const Project = mongoose.model("Project", projectSchema);
