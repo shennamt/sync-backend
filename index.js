f9a42ff91b1d263cbcbc7a60c92d70039733c6db
aaronyeo007 <aaron.yeo@icloud.com>
2023-03-27 19:42:26 +0800

    chore: - re-factor users.js with router

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 const cors = require("cors");
 
+const usersController = require("./controllers/users.js");
+
 // Include the method-override package
 const methodOverride = require("method-override");
 
@@ -29,7 +31,6 @@ const corsOptions = {
 //   },
 // };
 
-const User = require("./models/userModel.js");
 const Project = require("./models/Project.js");
 
 // Middleware; //////////////////////////////////////////////////////////////////
@@ -39,6 +40,9 @@ app.use(express.json());
 app.use(methodOverride("_method")); // for update and delete purpose
 
 app.use(cors(corsOptions));
+
+app.use("/users", usersController);
+
 //listen; //////////////////////////////////////////////////////////////////
 
 //connection
@@ -64,107 +68,6 @@ app.use("/api/user", userRoute);
 app.get("/", async (req, res) => {
   try {
     res.render("main.ejs");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Display/Read Index
-app.get("/users", async (req, res) => {
-  try {
-    const allUsers = await User.find({});
-    res.render("indexUsers.ejs", { users: allUsers });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Display/Read New User
-app.get("/users/new", (req, res) => {
-  res.render("newUsers.ejs");
-});
-
-// USER - Create New User
-app.post("/users/", async (req, res) => {
-  if (req.body.student === "on") {
-    // if checked, req.body.student is set to 'on'
-    req.body.student = true;
-  } else {
-    // if not checked, req.body.student is undefined
-    req.body.student = false;
-  }
-  if (req.body.professional === "on") {
-    // if checked, req.body.professional is set to 'on'
-    req.body.professional = true;
-  } else {
-    // if not checked, req.body.professional is undefined
-    req.body.professional = false;
-  }
-  try {
-    const user = await User.create(req.body);
-    // console.log(user);
-    res.redirect("/users");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Display/Read Each User
-app.get("/users/:id", async (req, res) => {
-  try {
-    const foundUser = await User.findById(req.params.id);
-    res.render("showUsers.ejs", {
-      user: foundUser
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Display Edit User Page
-app.get("/users/:id/edit", async (req, res) => {
-  try {
-    const foundUser = await User.findById(req.params.id);
-    res.render("editUsers.ejs", {
-      user: foundUser // pass in found project
-    });
-    console.log("foundUser -", foundUser);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Update User
-app.put("/users/:id", async (req, res) => {
-  if (req.body.student === "on") {
-    req.body.student = true;
-  } else {
-    req.body.student = false;
-  }
-  if (req.body.professional === "on") {
-    req.body.professional = true;
-  } else {
-    req.body.professional = false;
-  }
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body
-      // { new: true }
-    );
-    console.log("updatedUser -", updatedUser);
-    res.redirect("/users");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// USER - Delete User
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const removeUser = await User.findByIdAndDelete(req.params.id);
-    console.log(removeUser);
-    res.redirect("/users"); // redirect back to users index
   } catch (error) {
     console.log(error);
   }
