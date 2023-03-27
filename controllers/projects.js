478ea13ec5731e53afae03f69fd4f5d74d408b58
aaronyeo007 <aaron.yeo@icloud.com>
2023-03-27 19:49:40 +0800

    chore: - re-factored projects.js with router

---
+++ controllers/projects.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const Project = require("../models/Project.js");
+
+// PROJECT - Display/Read Index
+app.get("/", async (req, res) => {
+  try {
+    const allProjects = await Project.find({});
+    res.render("indexProjects.ejs", { projects: allProjects });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PROJECT - Display/Read New Project
+app.get("/new", (req, res) => {
+  res.render("newProjects.ejs");
+});
+
+// PROJECT - Create New Project
+app.post("/projects/", async (req, res) => {
+  if (req.body.agile === "on") {
+    // if checked, req.body.agile is set to 'on'
+    req.body.agile = true;
+  } else {
+    // if not checked, req.body.agile is undefined
+    req.body.agile = false;
+  }
+  if (req.body.kanban === "on") {
+    // if checked, req.body.kanban is set to 'on'
+    req.body.kanban = true;
+  } else {
+    // if not checked, req.body.kanban is undefined
+    req.body.kanban = false;
+  }
+  try {
+    const project = await Project.create(req.body);
+    // console.log(project);
+    res.redirect("/projects");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PROJECT - Display/Read Each Project
+app.get("/projects/:id", async (req, res) => {
+  try {
+    const foundProject = await Project.findById(req.params.id);
+    // res.send(foundProject);
+    res.render("showProjects.ejs", {
+      project: foundProject
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PROJECT - Display Edit User Page
+app.get("/projects/:id/edit", async (req, res) => {
+  try {
+    const foundProject = await Project.findById(req.params.id);
+    res.render("editProjects.ejs", {
+      project: foundProject // pass in found project
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PROJECT - Update Project
+app.put("/projects/:id", async (req, res) => {
+  if (req.body.agile === "on") {
+    req.body.agile = true;
+  } else {
+    req.body.agile = false;
+  }
+  if (req.body.kanban === "on") {
+    req.body.kanban = true;
+  } else {
+    req.body.kanban = false;
+  }
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(
+      req.params.id,
+      req.body
+      // { new: true }
+    );
+    // res.send(updatedProject);
+    res.redirect("/projects");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PROJECT - Delete Project
+app.delete("/projects/:id", async (req, res) => {
+  try {
+    const removeProject = await Project.findByIdAndDelete(req.params.id);
+    console.log(removeProject);
+    res.redirect("/projects"); // redirect back to projects index
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT;
 const cors = require("cors");
 
 const usersController = require("./controllers/users.js");
+const projectsController = require("./controllers/projects.js");
 
 // Include the method-override package
 const methodOverride = require("method-override");
@@ -42,6 +43,7 @@ app.use(methodOverride("_method")); // for update and delete purpose
 app.use(cors(corsOptions));
 
 app.use("/users", usersController);
+app.use("/projects", projectsController);
 
 //listen; //////////////////////////////////////////////////////////////////
 
@@ -68,107 +70,6 @@ app.use("/api/user", userRoute);
 app.get("/", async (req, res) => {
   try {
     res.render("main.ejs");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Display/Read Index
-app.get("/projects", async (req, res) => {
-  try {
-    const allProjects = await Project.find({});
-    res.render("indexProjects.ejs", { projects: allProjects });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Display/Read New Project
-app.get("/projects/new", (req, res) => {
-  res.render("newProjects.ejs");
-});
-
-// PROJECT - Create New Project
-app.post("/projects/", async (req, res) => {
-  if (req.body.agile === "on") {
-    // if checked, req.body.agile is set to 'on'
-    req.body.agile = true;
-  } else {
-    // if not checked, req.body.agile is undefined
-    req.body.agile = false;
-  }
-  if (req.body.kanban === "on") {
-    // if checked, req.body.kanban is set to 'on'
-    req.body.kanban = true;
-  } else {
-    // if not checked, req.body.kanban is undefined
-    req.body.kanban = false;
-  }
-  try {
-    const project = await Project.create(req.body);
-    // console.log(project);
-    res.redirect("/projects");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Display/Read Each Project
-app.get("/projects/:id", async (req, res) => {
-  try {
-    const foundProject = await Project.findById(req.params.id);
-    // res.send(foundProject);
-    res.render("showProjects.ejs", {
-      project: foundProject
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Display Edit User Page
-app.get("/projects/:id/edit", async (req, res) => {
-  try {
-    const foundProject = await Project.findById(req.params.id);
-    res.render("editProjects.ejs", {
-      project: foundProject // pass in found project
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Update Project
-app.put("/projects/:id", async (req, res) => {
-  if (req.body.agile === "on") {
-    req.body.agile = true;
-  } else {
-    req.body.agile = false;
-  }
-  if (req.body.kanban === "on") {
-    req.body.kanban = true;
-  } else {
-    req.body.kanban = false;
-  }
-  try {
-    const updatedProject = await Project.findByIdAndUpdate(
-      req.params.id,
-      req.body
-      // { new: true }
-    );
-    // res.send(updatedProject);
-    res.redirect("/projects");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// PROJECT - Delete Project
-app.delete("/projects/:id", async (req, res) => {
-  try {
-    const removeProject = await Project.findByIdAndDelete(req.params.id);
-    console.log(removeProject);
-    res.redirect("/projects"); // redirect back to projects index
   } catch (error) {
     console.log(error);
   }
