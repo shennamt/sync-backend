f8feddcd1be39209757104ec31554f5a2ce6c513
aaronyeo007 <aaron.yeo@icloud.com>
2023-03-31 17:47:05 +0800

    bugfix: - able to import missing userId into projectController upon login

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,22 +1,27 @@
-const Project = require('../models/projectModel')
+const Project = require("../models/projectModel");
+const userController = require("./userController");
 
 // POST
 // find the number of existing projects in db with count
 // creates new projects with user who auth request, and position of new proj in list
 // success: 201 created res with new proj {} as JSON
 // error: 500 internal server error res with error {} as JSON
+let userId = "";
+
 const create = async (req, res) => {
+  userId = userController.userId;
+  console.log("projectController.js - create function - userId:\n", userId);
   try {
-    const projectsCount = await Project.find().count()
+    const projectsCount = await Project.find().count();
     const project = await Project.create({
-      user: req.user._id,
+      user: userId,
       position: projectsCount > 0 ? projectsCount : 0
-    })
-    res.status(201).json(project)
+    });
+    res.status(201).json(project);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-}
+};
 
 // GET
 // final all proj belonging to auth user
@@ -26,11 +31,12 @@ const create = async (req, res) => {
 // error: 500 internal server error with error {} as JSON
 const getAll = async (req, res) => {
   try {
-    const projects = await Project.find({ user: req.user._id }).sort('-position')
-    res.status(200).json(projects)
+    userId = userController.userId;
+    const projects = await Project.find({ user: userId }).sort("-position");
+    res.status(200).json(projects);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-}
+};
 
-module.exports = { create, getAll }
+module.exports = { create, getAll };
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -20,6 +20,7 @@ const loginUser = async (req, res) => {
     console.log("Backend userController.js - token:\n", token);
 
     res.status(200).json({ email, token, userId });
+    module.exports.userId = userId;
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
