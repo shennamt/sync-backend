7eda9dcd541c50fd3756a6a62edaf34c128e5904
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 07:56:40 +0800

    feat: board update router added

--- src/v1/routes/board.js
+++ src/v1/routes/board.js
@@ -5,7 +5,7 @@ const tokenHandler = require("../handlers/tokenHandler");
 const boardController = require("../controllers/board");
 
 router.post("/", tokenHandler.verifyToken, boardController.create);
-
 router.get("/", tokenHandler.verifyToken, boardController.getAll);
+router.put("/", tokenHandler.verifyToken, boardController.updatePosition);
 
 module.exports = router;
