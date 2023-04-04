0493cb913327fdb5abbf857c36686990707a2d35
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 08:27:14 +0800

    feat: get route for board routes updated

--- src/v1/routes/board.js
+++ src/v1/routes/board.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
-const { body } = require("express-validator");
+const { param } = require("express-validator");
 const validation = require("../handlers/validation");
 const tokenHandler = require("../handlers/tokenHandler");
 const boardController = require("../controllers/board");
 
 router.post("/", tokenHandler.verifyToken, boardController.create);
+
 router.get("/", tokenHandler.verifyToken, boardController.getAll);
+
 router.put("/", tokenHandler.verifyToken, boardController.updatePosition);
+
+router.get(
+  '/:boardId',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid id')
+    }
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  boardController.getOne
+)
 
 module.exports = router;
