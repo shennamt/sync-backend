e5a1852e84b773f13c71070aefe9307c3766b21a
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 08:22:39 +0800

    feat: boards put req added to board controller

--- src/v1/controllers/board.js
+++ src/v1/controllers/board.js
@@ -41,3 +41,20 @@ exports.updatePosition = async (req, res) => { // PUT req to update position
     res.status(500).json(err)
   }
 }
+
+exports.getOne = async (req, res) => { // GET rew for specific board by ID
+  const { boardId } = req.params
+  try {
+    const board = await Board.findOne({ user: req.user._id, _id: boardId }) // based on board and user id
+    if (!board) return res.status(404).json('Board not found')
+    const sections = await Section.find({ board: boardId }) // if found, retrieves sections
+    for (const section of sections) {
+      const tasks = await Task.find({ section: section.id }).populate('section').sort('-position') // for each section, find tasks
+      section._doc.tasks = tasks // doc is a mongoose prop. access plain js {}.
+    }
+    board._doc.sections = sections
+    res.status(200).json(board)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
