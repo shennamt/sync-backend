6a67c59123d79cdfa1ee9a20ffc4e88465b39ad8
aaronyeo007 <aaron.yeo@icloud.com>
2023-04-04 18:08:39 +0800

    feature: - added section, changes on-going

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth"));
 router.use("/boards", require("./board"));
+router.use("/boards/:boardId/sections", require("./section"));
+// router.use("/boards/:boardId/tasks", require("./task"));
 
 module.exports = router;
