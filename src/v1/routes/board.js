f85eb2339b72bc571e51b17c82b7e4f340caa06f
aaronyeo007 <aaron.yeo@icloud.com>
2023-04-07 18:01:22 +0800

    bugfix: fixed updating of board title

--- src/v1/routes/board.js
+++ src/v1/routes/board.js
@@ -4,6 +4,12 @@ const validation = require("../handlers/validation");
 const tokenHandler = require("../handlers/tokenHandler");
 const boardController = require("../controllers/board");
 
+router.post("/", tokenHandler.verifyToken, boardController.create);
+
+router.get("/", tokenHandler.verifyToken, boardController.getAll);
+
+router.put("/", tokenHandler.verifyToken, boardController.updatePosition);
+
 /* prettier-ignore */
 router.get(
   '/:boardId',
@@ -17,6 +23,20 @@ router.get(
   boardController.getOne
   )
 
+router.put(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!validation.isObjectId(value)) {
+      // cause the validation to fail and return a 400 Bad Request
+      //  response to the client
+      return Promise.reject("invalid id");
+    } else return Promise.resolve(); // BUG FIXED - 404 Not Found
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  boardController.update
+);
+
 router.delete(
   "/:boardId",
   param("boardId").custom((value) => {
@@ -29,9 +49,4 @@ router.delete(
   boardController.getOne
 );
 
-router.post("/", tokenHandler.verifyToken, boardController.create);
-
-router.get("/", tokenHandler.verifyToken, boardController.getAll);
-
-router.put("/", tokenHandler.verifyToken, boardController.updatePosition);
 module.exports = router;
--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth"));
 router.use("/boards", require("./board"));
-router.use("/boards/:boardId", require("./section"));
 router.use("/boards/:boardId/sections", require("./section"));
 // router.use("/boards/:boardId/tasks", require("./task"));
 
