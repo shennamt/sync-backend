7d24430fdbe4c22053488216a2b4a74d9f98e632
aaronyeo007 <aaron.yeo@icloud.com>
2023-04-29 15:01:04 +0800

    feature: added tasks

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 router.use("/auth", require("./auth"));
 router.use("/boards", require("./board"));
 router.use("/boards/:boardId/sections", require("./section"));
-// router.use("/boards/:boardId/tasks", require("./task"));
+router.use("/boards/:boardId/tasks", require("./task"));
 
 module.exports = router;
